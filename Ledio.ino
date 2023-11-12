a40f3f6af1e88c61bf7f3150b49fd447d3cfc7cc
Sadhasivam <106687593+SadhaSivamx@users.noreply.github.com>
2023-11-12 17:52:46 +0530

    Create Ledio.ino

---
+++ Ledio.ino
@@ -0,0 +1,21 @@
+int ledPin = 13; // Define the LED pin as pin 13
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // Set the LED pin as an output
+  Serial.begin(9600); // Initialize serial communication
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read(); // Read the command from PySerial
+    
+    if (command == '1') {
+      digitalWrite(ledPin, HIGH); // Turn on the LED
+      Serial.println("LED is ON");
+    }
+    else if (command == '0') {
+      digitalWrite(ledPin, LOW); // Turn on the LED
+      Serial.println("LED is OFF");
+    }
+  }
+}
